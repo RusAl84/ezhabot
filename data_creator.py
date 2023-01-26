e30a58db4b6a533266e5ed1f11c3d6e60f107596
Aleksey <rusal@bk.ru>
2023-01-26 17:23:52 +0300

    add json data categories

---
+++ data_creator.py
@@ -0,0 +1,31 @@
+﻿categories = []
+item_cat1={}
+item_cat1["id"]= 1
+item_cat1["name"]= "Хобби"
+item_cat1["desc"]= "Ваши увлечения"
+categories.append(item_cat1)
+item_cat2={}
+item_cat2["id"]= 2
+item_cat2["name"]= "Домашние животные"
+item_cat2["desc"]= "Увлечения с вашими любимцами"
+categories.append(item_cat2)
+import json
+jsonstring = json.dumps(categories, ensure_ascii=False)
+with open('categories.json', 'w', encoding='utf-8') as f:
+    f.write(jsonstring)
+    
+sub_categories = []
+item_sub_cat1={}
+item_sub_cat1["parent_id"]= 1
+item_sub_cat1["name"]= "Коллекционирование марок"
+item_sub_cat1["desc"]= "История и оценки марок"
+sub_categories.append(item_sub_cat1)
+item_sub_cat2={}
+item_sub_cat2["parent_id"]= 1
+item_sub_cat2["name"]= "Философия"
+item_sub_cat2["desc"]= "Обсуждают материалы по философии"
+sub_categories.append(item_sub_cat2)
+import json
+jsonstring = json.dumps(categories, ensure_ascii=False)
+with open('sub_categories.json', 'w', encoding='utf-8') as f:
+    f.write(jsonstring)
--- ezhabot_1.py
+++ ezhabot_1.py
@@ -1,6 +1,6 @@
 import telebot;
 
-bot = telebot.TeleBot('5982175377:AAH4V5oGT1kmyCKnrLo--w9RHvEJLW7DI9s');
+bot = telebot.TeleBot('5982175377:AAG9_9nyODx2tgpqg5qYgKqAIuhUdhPukTg');
 
 
 @bot.message_handler(content_types=['text'])
